670f4eff8c612b62c2c2091008ee523b95660d88
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-04 09:24:57 +0200

    listas

---
+++ python3/operadores.py
@@ -0,0 +1,41 @@
+Operadores aritmeticos
+
+( ) = parentesis
+= multiplicacion
+/ = division
+% = modulo
+= suma
+– = resta
+** = potencia
+Operadores racionales
+
+= mayor que
+
+< = menor que
+= = mayor o igual que
+
+= = menor o igual que
+
+!= = diferente
+== = igualdad
+= -> igual
+Operadores lógicos
+
+Not
+And
+Or
+Prioridad de operadores en funcionalidad
+
+()
+**
+*,/, not
++, -, and
+,<,==,>=,<=, ! , or
+
+Operadores de asignación
+a + = 5 Suma en Asignación
+b - = 2 resta en Asignación
+a * = 3 multiplicación en Asignación
+a / = 3 división en Asignación
+a ** = 2 potencia en Asignación
+a % = 2 modulo en Asignación
