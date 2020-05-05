96515c5e8edd248ecdae85f50b3b851be83c079d
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-05 10:06:34 +0200

    flask--> template jinja2, estructuras de control, herencia de templates

--- Flask/main.py
+++ Flask/main.py
@@ -1,10 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Compar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
-    return 'Hello World Software Engineers tu IP es {}'.format(user_ip)
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+
+    return render_template('hello.html', **context)#los dos ** despliega os contextos en todo el directorio
 
