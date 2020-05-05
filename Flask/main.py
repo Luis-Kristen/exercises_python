af22b8f1a1833f545e7bda57fc81cbf36401b9ba
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-05 13:54:02 +0200

    flask--> Flask-Bootstrap y Flask-WTF render de forms y instalacion Flask-WTF

--- Flask/main.py
+++ Flask/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -9,6 +12,10 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Compar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    Password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -35,9 +42,11 @@ def index():
 def hello():
     # user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)#los dos ** despliega os contextos en todo el directorio
