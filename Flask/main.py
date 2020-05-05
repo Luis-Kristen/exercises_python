b4c943ac3362c512b98e6cf4b785ab72f7310fac
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-05 16:49:30 +0200

    Desplegar Flashes, mensajes emergentes

--- Flask/main.py
+++ Flask/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -10,12 +11,14 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
-todos = ['Compar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
 
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
-    Password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -32,7 +35,6 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    # response.set_cookie('user_ip', user_ip) quitamos la cookie para que no quede expuesta
     session['user_ip'] = user_ip
 
     return response
@@ -40,7 +42,6 @@ def index():
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    # user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
     login_form = LoginForm()
     username = session.get('username')
@@ -56,7 +57,8 @@ def hello():
         username = login_form.username.data
         session['username'] = username
 
+        flash('Nombre de usario registrado con éxito!')
+
         return redirect(url_for('index'))
 
-    return render_template('hello.html', **context)#los dos ** despliega os contextos en todo el directorio
-
+    return render_template('hello.html', **context)
