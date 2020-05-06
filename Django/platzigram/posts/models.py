8c9771e9cfd766f29c2a7734c52600a295d35a40
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-06 19:47:56 +0200

    el ORM de Django db sqlite3

--- Django/platzigram/posts/models.py
+++ Django/platzigram/posts/models.py
@@ -1,17 +1,19 @@
 """Posts models."""
 
-# Django
+#Â Django
 from django.db import models
 
 
 class User(models.Model):
     """User model."""
-    
+
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
 
     bio = models.TextField(blank=True)
 
@@ -19,3 +21,7 @@ class User(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return email."""
+        return self.email
