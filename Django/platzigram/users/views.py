eedb4f64e411745f094d6422f1dc41cdb61e4092
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-07 12:33:25 +0200

    Middleware, requisito estas authenticado con profile o foto para mostar el feed

--- Django/platzigram/users/views.py
+++ Django/platzigram/users/views.py
@@ -8,9 +8,14 @@ from django.shortcuts import render, redirect
 # Exception
 from django.db.utils import IntegrityError
 
-# Models
+# Models 
 from django.contrib.auth.models import User
 from users.models import Profile
+
+
+def update_profile(request):
+    """Update a user's profile view."""
+    return render(request, 'users/update_profile.html')
 
 
 def login_view(request):
