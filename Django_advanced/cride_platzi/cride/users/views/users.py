7aa280c28f5a65fc8abb00f8049d503f86993f1d
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-13 09:37:26 +0200

    User sign up

--- Django_advanced/cride_platzi/cride/users/views/users.py
+++ Django_advanced/cride_platzi/cride/users/views/users.py
@@ -8,9 +8,9 @@ from rest_framework.views import APIView
 # Serializers
 from cride.users.serializers import (
     UserLoginSerializer,
-    UserModelSerializer
+    UserModelSerializer,
+    UserSignUpSerializer,
 )
-
 
 class UserLoginAPIView(APIView):
     """User Login API View"""
@@ -24,4 +24,16 @@ class UserLoginAPIView(APIView):
             'user': UserModelSerializer(user).data,
             'acces_token': token
         }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class UserSignUpAPIView(APIView):
+    """User Signup API View"""
+    
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST request """
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
