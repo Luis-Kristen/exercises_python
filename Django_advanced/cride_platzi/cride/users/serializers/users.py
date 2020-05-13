e32c61c3d94251f11a8969d313e4da56e84872c3
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-13 12:45:33 +0200

    Verificar cuenta usando JWT

--- Django_advanced/cride_platzi/cride/users/serializers/users.py
+++ Django_advanced/cride_platzi/cride/users/serializers/users.py
@@ -101,7 +101,7 @@ class UserSignUpSerializer(serializers.Serializer):
 
     def gen_verification_token(self, user):
         """Create JWT token that the user can use to verify its account """
-        exp_date = timezone.now() + timedelta(seconds=1)
+        exp_date = timezone.now() + timedelta(minutes=30)
         payload = {
             'user': user.username,
             'exp': int(exp_date.timestamp()),
@@ -135,3 +135,29 @@ class UserLoginSerializer(serializers.Serializer):
         token, created = Token.objects.get_or_create(user=self.context['user'])
         return self.context['user'], token.key
 
+
+class AccountVerificationSerializer(serializers.Serializer):
+    """Account verification serializer"""
+
+    token = serializers.CharField()
+
+    def validate_token(self, data):
+        """Verify token is valid"""
+        try:
+            payload = jwt.decode(data, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise serializers.ValidationError('Verification link has expired.')
+        except jwt.PyJWTError:
+            raise serializers.ValidationError('Invalid token')
+        if payload['type'] != 'email_confirmation':
+            raise serializers.ValidationError('Invalid Token')
+
+        self.context['payload'] = payload
+        return data
+
+    def save(self):
+        """Update user´s verified status"""
+        payload = self.context['payload']
+        user = User.objects.get(username=payload['user'])
+        user.is_verified = True
+        user.save()
--- Django_advanced/cride_platzi/cride/users/views/users.py
+++ Django_advanced/cride_platzi/cride/users/views/users.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 
 # Serializers
 from cride.users.serializers import (
+    AccountVerificationSerializer,
     UserLoginSerializer,
     UserModelSerializer,
     UserSignUpSerializer,
@@ -37,3 +38,15 @@ class UserSignUpAPIView(APIView):
         user = serializer.save()
         data = UserModelSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class AccountVerificationAPIView(APIView):
+    """Account verification API View"""
+    
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST request """
+        serializer = AccountVerificationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        data = {'message': 'congratulations, now go share some rides!'}
+        return Response(data, status=status.HTTP_200_OK)
