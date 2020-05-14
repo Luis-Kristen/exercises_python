1a70553dd7f60f37b51a2d9df75c764bdefc6026
Luis Carlos Kristen <luiskristen@gmail.com>
2020-05-14 19:58:54 +0200

    Retrieve destroy member

--- Django_advanced/cride_platzi/cride/circles/views/memberships.py
+++ Django_advanced/cride_platzi/cride/circles/views/memberships.py
@@ -7,11 +7,20 @@ from rest_framework.generics import get_object_or_404
 # Models
 from cride.circles.models import Circle, Membership
 
+# permissions
+from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.memberships import IsActiveCircleMember
+
 # Serializers
 from cride.circles.serializers import MembershipModelSerializer
 
 
-class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class MembershipViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     """Circle membership view set."""
 
     serializer_class = MembershipModelSerializer
@@ -22,9 +31,23 @@ class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         self.circle = get_object_or_404(Circle, slug_name=slug_name)
         return super(MembershipViewSet, self).dispatch(request, *args, **kwargs)
 
+    def get_permissions(self):
+        """Asign permissions based on action."""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        return [p() for p in permissions]
+
     def get_queryset(self):
         """Return circle members."""
         return Membership.objects.filter(
+            circle=self.circle,
+            is_active=True
+        )
+
+    def get_object(self):
+        """Return the circle member by using the user's username."""
+        return get_object_or_404(
+            Membership,
+            user__username=self.kwargs['pk'],
             circle=self.circle,
             is_active=True
         )
